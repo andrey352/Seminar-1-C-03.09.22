967a38a91294388617eff844f4442660e4cb873c
andrey352 <bauer350@mail.ru>
2022-09-03 13:03:20 +0300

    add dz

---
+++ dz/Program.cs
@@ -0,0 +1,65 @@
+﻿// 1
+
+// Console.Write("Enter a first integer: ");
+// int num1 = int.Parse(Console.ReadLine());
+
+// Console.Write("Enter a second integer: ");
+// int num2 = int.Parse(Console.ReadLine());
+
+// if(num1 > num2)
+// {
+//     Console.WriteLine($"Max integer - {num1}");
+// }
+
+// else
+// {
+//     Console.WriteLine($"Max integer - {num2}");
+// }
+
+// 2
+
+// Console.Write("Enter a first integer: ");
+// int a = int.Parse(Console.ReadLine());
+
+// Console.Write("Enter a second integer: ");
+// int b = int.Parse(Console.ReadLine());
+
+// Console.Write("Enter a third integer: ");
+// int c = int.Parse(Console.ReadLine());
+
+// int max = a;
+
+// if(a > max) max = a;
+// if(b > max) max = b;
+// if(c > max) max = c;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+// 3
+
+// Console.Write("Enter a enteger: ");
+// int num = int.Parse(Console.ReadLine());
+
+// if(num % 2 == 0)
+// {
+//     Console.WriteLine("Yes");
+// }
+
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+// 4
+
+// Console.Write("Enter a integer:");
+// int n = int.Parse(Console.ReadLine());
+
+// int startnum = 2;
+
+// while(startnum <= n)
+// {
+//     Console.WriteLine(startnum);
+//     startnum = startnum + 2;
+// }
